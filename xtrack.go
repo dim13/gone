87e3bd4d5a391bf8013388a3b6b58b483e64ccab
Dimitri Sokolyuk <demon@dim13.org>
2014-06-05 18:33:02 +0200

    stub, just logging

--- xtrack.go
+++ xtrack.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/BurntSushi/xgb"
 	"github.com/BurntSushi/xgb/xproto"
 	"log"
-	"time"
 )
+
+type window struct {
+	Class []string
+	Name  string
+}
+
+func asciizToString(b []byte) (s []string) {
+	for _, x := range bytes.Split(b, []byte{0}) {
+		s = append(s, string(x))
+	}
+	if len(s) > 0 && s[len(s)-1] == "" {
+		s = s[:len(s)-1]
+	}
+	return
+}
 
 func atom(X *xgb.Conn, aname string) *xproto.InternAtomReply {
 	a, err := xproto.InternAtom(X, true, uint16(len(aname)), aname).Reply()
@@ -23,19 +39,44 @@ func prop(X *xgb.Conn, w xproto.Window, a *xproto.InternAtomReply) *xproto.GetPr
 	return p
 }
 
-func winName(X *xgb.Conn) string {
-	setup := xproto.Setup(X)
-	root := setup.DefaultScreen(X).Root
+func (w window) String() string {
+	return fmt.Sprintf("%s (%s) %s", w.Class[0], w.Class[1], w.Name)
+}
 
+func winName(X *xgb.Conn, root xproto.Window) (window, bool) {
 	activeAtom := atom(X, "_NET_ACTIVE_WINDOW")
 	nameAtom := atom(X, "WM_NAME")
+	classAtom := atom(X, "WM_CLASS")
 
-	reply := prop(X, root, activeAtom)
-	windowId := xproto.Window(xgb.Get32(reply.Value))
+	active := prop(X, root, activeAtom)
+	windowId := xproto.Window(xgb.Get32(active.Value))
 
-	reply = prop(X, windowId, nameAtom)
+	/* skip root window */
+	if windowId == 0 {
+		return window{}, false
+	}
 
-	return string(reply.Value)
+	spy(X, windowId)
+
+	name := prop(X, windowId, nameAtom)
+	class := prop(X, windowId, classAtom)
+
+	w := window{
+		Class: asciizToString(class.Value),
+		Name:  string(name.Value),
+	}
+
+	return w, true
+}
+
+func rootWin(X *xgb.Conn) xproto.Window {
+	setup := xproto.Setup(X)
+	return setup.DefaultScreen(X).Root
+}
+
+func spy(X *xgb.Conn, w xproto.Window) {
+	xproto.ChangeWindowAttributes(X, w, xproto.CwEventMask,
+		[]uint32{xproto.EventMaskPropertyChange})
 }
 
 func main() {
@@ -45,10 +86,15 @@ func main() {
 	}
 	defer X.Close()
 
-	for {
-		//X.WaitForEvent()
-		log.Println(winName(X))
-		time.Sleep(time.Second)
+	root := rootWin(X)
+	spy(X, root)
 
+	for {
+		if _, everr := X.WaitForEvent(); everr != nil {
+			log.Fatal(err)
+		}
+		if name, ok := winName(X, root); ok {
+			log.Println(name)
+		}
 	}
 }
