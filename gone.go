4121773d54ad09c9f5be9e1dd4d208871ab98cf5
Dimitri Sokolyuk <demon@dim13.org>
2014-06-07 02:42:20 +0200

    reduce time of incativity to 6h

--- gone.go
+++ gone.go
@@ -283,7 +283,7 @@ func main() {
 	go tracks.collect()
 	go func() {
 		for {
-			tracks.cleanup(12 * time.Hour)
+			tracks.cleanup(6 * time.Hour)
 			tracks.store(file)
 			time.Sleep(time.Minute)
 		}
