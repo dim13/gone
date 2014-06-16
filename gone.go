0955d1a94e7e32b085eedb26b4b2968987928e13
Dimitri Sokolyuk <demon@dim13.org>
2014-06-16 22:26:36 +0200

    fix overcounting: simple set idle time

--- gone.go
+++ gone.go
@@ -66,7 +66,7 @@ func (t Tracks) Snooze(idle time.Duration) {
 	if !zzz {
 		logger.Println("away from keyboard, idle for", idle)
 		if c, ok := t[current]; ok {
-			c.Idle += idle
+			c.Idle = idle
 			t[current] = c
 		}
 		zzz = true
