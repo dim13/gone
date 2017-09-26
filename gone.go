9dd7096afcaee31a18fd377688da86a5cf885938
Dimitri Sokolyuk <demon@dim13.org>
2017-09-27 00:13:45 +0200

    Less globals

--- gone.go
+++ gone.go
@@ -26,11 +26,6 @@ type Track struct {
 	Idle  time.Duration
 }
 
-var (
-	dumpFileName = path.Join(CachePath(), "gone.gob")
-	logFileName  = path.Join(CachePath(), "gone.log")
-)
-
 func (t Track) String() string {
 	return fmt.Sprintf("%s %s", t.Seen.Format("2006/01/02 15:04:05"), t.Spent)
 }
@@ -123,40 +118,42 @@ func (t Tracks) Store(fname string) {
 	os.Rename(tmp, fname)
 }
 
-func (t Tracks) Cleanup(every, since time.Duration) {
+func (t Tracks) Cleanup(every, since time.Duration, dump string) {
 	tick := time.NewTicker(every)
 	defer tick.Stop()
 	for range tick.C {
 		t.RemoveSince(since)
-		t.Store(dumpFileName)
+		t.Store(dump)
 	}
 }
 
 func main() {
 	var (
-		display = flag.String("display", os.Getenv("DISPLAY"), "X11 display")
-		listen  = flag.String("listen", "127.0.0.1:8001", "web reporter")
-		timeout = flag.Duration("timeout", time.Minute*5, "idle timeout")
-		expire  = flag.Duration("expire", time.Hour*8, "expire timeout")
-		refresh = flag.Duration("refresh", time.Minute, "refresh interval")
+		display  = flag.String("display", os.Getenv("DISPLAY"), "X11 display")
+		listen   = flag.String("listen", "127.0.0.1:8001", "web reporter")
+		timeout  = flag.Duration("timeout", time.Minute*5, "idle timeout")
+		expire   = flag.Duration("expire", time.Hour*8, "expire timeout")
+		refresh  = flag.Duration("refresh", time.Minute, "refresh interval")
+		logFile  = flag.String("logfile", path.Join(CachePath(), "gone.log"), "log file")
+		dumpFile = flag.String("dumpfile", path.Join(CachePath(), "gone.gob"), "dump file")
 	)
 	flag.Parse()
 
 	X := Connect(*display)
 	defer X.Close()
 
-	logfile, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	logfile, err := os.OpenFile(*logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer logfile.Close()
 
-	tracks := Load(dumpFileName)
+	tracks := Load(*dumpFile)
 	tracks.logger = log.New(logfile, "", log.LstdFlags)
-	defer tracks.Store(dumpFileName)
+	defer tracks.Store(*dumpFile)
 
 	go X.Collect(tracks, *timeout)
-	go tracks.Cleanup(*refresh, *expire)
+	go tracks.Cleanup(*refresh, *expire, *dumpFile)
 
 	if err := webReporter(tracks, *listen); err != nil {
 		log.Fatal(err)
