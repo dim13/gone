dd3b689075c2079b7c433c4c593eacc9f05998d5
Dimitri Sokolyuk <demon@dim13.org>
2017-10-04 15:57:58 +0200

    tweak js

--- static/app.js
+++ static/app.js
@@ -19,7 +19,7 @@ function loadTracks() {
 
 function storeTracks(tracks) {
 	tracks.sort(function(a, b) {
-		return a.Active - b.Active;
+		return b.Active - a.Active;
 	});
 	localStorage.setItem("tracks", JSON.stringify(tracks));
 	return tracks;
@@ -49,14 +49,18 @@ function update(data) {
 }
 
 function display(tracks) {
-	var tab = document.getElementById("table");
-	tab.innerHTML = "";
+	var table = document.createElement("table");
+	var head = table.createTHead().insertRow(0);
+	head.insertCell(0).innerHTML = "Class";
+	head.insertCell(1).innerHTML = "Name";
+	head.insertCell(2).innerHTML = "Spent";
 	tracks.map(function(item) {
-		var row = tab.insertRow(0);
+		var row = table.insertRow(-1);
 		row.insertCell(0).innerHTML = item.Class;
 		row.insertCell(1).innerHTML = item.Name;
 		row.insertCell(2).innerHTML = duration(item.Active);
 	});
+	document.getElementById("table").innerHTML = table.innerHTML;
 }
 
 function clearStorage() {
